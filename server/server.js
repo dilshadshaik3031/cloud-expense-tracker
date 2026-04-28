f09c8904ec2d506ea868760c13b6f1bf068d35cf
dilshad <dilshadshaik3031@gmail.com>
2026-04-28 17:22:20 -0500

    Integrate MongoDB Atlas with Express backend

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Cloud Expense Tracker API is running');
+app.get("/", (req, res) => {
+  res.send("Cloud Expense Tracker API Running");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
